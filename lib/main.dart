e4c121b5153fc2638ee0cbc9d02851e9281eee29
duanmingming <geekdmm@163.com>
2019-08-28 21:28:16 +0800

    添加 BarButtonItem

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'demo/listview_demo.dart';
 
 void main(List<String> args) => runApp(App());
 
-
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -19,7 +18,21 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.grey[100],
-        appBar: AppBar(title: Text('我是标题')),
-        body: ListViewDemo());
+        appBar: AppBar(
+          title: Text('我是标题'),
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: "menu",
+            onPressed: () => debugPrint('tap menu'),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'search',
+              onPressed: () => debugPrint('search'),
+            )
+          ],
+        ),
+        body: null);
   }
 }
