bc0a5eeeefbe344b0ab4ed8d3987d404f22eec78
duanmingming <geekdmm@163.com>
2019-08-28 23:08:51 +0800

    添加 tb

--- lib/main.dart
+++ lib/main.dart
@@ -16,23 +16,56 @@ class App extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.grey[100],
-        appBar: AppBar(
-          title: Text('我是标题'),
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: "menu",
-            onPressed: () => debugPrint('tap menu'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            title: Text('我是标题'),
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: "menu",
+              onPressed: () => debugPrint('tap menu'),
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: 'search',
+                onPressed: () => debugPrint('search'),
+              )
+            ],
+            bottom: TabBar(
+              unselectedLabelColor: Colors.black38,
+              indicatorColor: Colors.black54,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorWeight: 1.0,
+              tabs: <Widget>[
+                Icon(
+                  Icons.access_alarm,
+                ),
+                Icon(
+                  Icons.accessible_forward,
+                ),
+                Icon(
+                  Icons.assistant_photo,
+                ),
+              ],
+            ),
           ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'search',
-              onPressed: () => debugPrint('search'),
-            )
-          ],
-        ),
-        body: null);
+          body: TabBarView(children: <Widget>[
+            Icon(
+              Icons.access_alarm,
+              size: 128.0,
+            ),
+            Icon(
+              Icons.accessible_forward,
+              size: 128.0,
+            ),
+            Icon(
+              Icons.assistant_photo,
+              size: 128.0,
+            ),
+          ],)),
+    );
   }
 }
